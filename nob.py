3e94f11cbf644b36b20cac66fabbe07178320528
SnowsSky <exaltypixou@gmail.com>
2026-01-09 22:15:26 +0100

    Updated nob to 1.4.0
    nob 1.4.0
    - Added multi-install / remove, so now you can do 'nob -S pkg1 pkg2' + rework of 'nob -S' & 'nob -R'.
    - Added -U option, you can now install packages on cache with 'nob -U'
    - Added cache for pkgs.
    - Added pyalpm version on 'nob -v'
    - Rework of 'nob -Sa'
    - Added multi packages build

--- nob.py
+++ nob.py
@@ -12,31 +12,37 @@ import random
 from colors import Colors 
 from TUI import TUI
 colors = Colors()
+import shutil
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-S", type=str, required=False, help=f"Install a package from AUR" ,dest="install")
+    parser.add_argument("-S", type=str, nargs='+', required=False, help=f"Install a package from AUR" ,dest="install")
     parser.add_argument("-Ss", type=str, required=False, help=f"Search a package from AUR" ,dest="search")
-    parser.add_argument("-Sb", type=str, required=False, help=f"Build a package (but not installing it.)" ,dest="build")
+    parser.add_argument("-Sb", type=str, nargs='+', required=False, help=f"Build a package (but not installing it.)" ,dest="build")
     parser.add_argument("-Sa", action='store_true', required=False, help="Upgrades AUR packages", dest="aur_upgrade")
+    parser.add_argument("-U", type=str, nargs='+', required=False, help="localy installing a AUR package", dest="local_install")
     parser.add_argument("-Sr", action='store_true', required=False, help="Install a random package from AUR", dest="install_random") # [WARNING] : I am no responsible for any damage caused by this feature, this will download a random package from the AUR.
     parser.add_argument("-Qa", action='store_true', required=False, help="Show all packages installed with nob", dest="show_installed_aur_pkgs")
     parser.add_argument("--auto-detect", action='store_true', required=False, help="Auto detection for installed AUR packages with another AUR helper", dest="auto_detect")
+    parser.add_argument("--clean-cache", action='store_true', required=False, help="Clean the cache of nob", dest="clear_cache")
     parser.add_argument("--noconfirm", action='store_true', required=False, help="No confirmations", dest="noconfirm")
     parser.add_argument("-arch-update-settings", action='store_true', required=False, help="TUI for arch-update settings", dest="arch_update_settings")
     parser.add_argument("-v", action='store_true', required=False, help="See the current version of nob", dest="nob_version")
-    parser.add_argument('-R', type=str, required=False, help=f'Remove a package.',dest="remove")
-    parser.add_argument('-Rns', type=str, required=False, help=f'Remove a package.',dest="remove")
-    parser.add_argument('-Rsn', type=str, required=False, help=f'Remove a package.',dest="remove")
-    parser.add_argument('-Rs', type=str, required=False, help=f'Remove a package.',dest="remove")
-    parser.add_argument('-Rn', type=str, required=False, help=f'Remove a package.',dest="remove")
+    parser.add_argument('-R', type=str, nargs='+', required=False, help=f'Remove a package.',dest="remove")
+    parser.add_argument('-Rns', type=str, nargs='+', required=False, help=f'Remove a package.',dest="remove")
+    parser.add_argument('-Rsn', type=str, nargs='+', required=False, help=f'Remove a package.',dest="remove")
+    parser.add_argument('-Rs', type=str, nargs='+', required=False, help=f'Remove a package.',dest="remove")
+    parser.add_argument('-Rn', type=str, nargs='+', required=False, help=f'Remove a package.',dest="remove")
 
     return parser.parse_args()
 
-_version = "1.3.0"
+_version = "1.4.0"
+_pyalpm_version = libalpm.alpm.version()
 
 args = parse_args()
 Install_URL = f"https://aur.archlinux.org/rpc.php?v=5&type=info&arg={args.install}"
 pckg_archive = f"{args.install}.git"
+cache_folder = '/var/cache/nob/pkgs/'
 folder = f"/home/{os.getlogin()}"
 os.chdir(folder)
 #Check is running as root
@@ -53,31 +59,72 @@ def detect_pkgs():
     print(f"{colors.GREEN}==>{colors.END} Detection completed.")
 
 def build_only():
-    result = download_find_pkg(args.build)
-    if result == None: return
-    _, pkg_popularity = result
-    if pkg_popularity <= 2:
-        print(f"{colors.YELLOW}==> WARNING{colors.END} : This package popularity is LOW ({pkg_popularity}), this may be an indesirable program.)")
+    to_build = []
+    for pkg in args.build:
+
+        result = download_find_pkg(pkg, True)
+        if result == None: return
+        _, pkg_popularity = result
+        if pkg_popularity <= 2:
+            print(f"{colors.YELLOW}==> WARNING{colors.END} : {pkg} popularity is LOW ({pkg_popularity}), this may be an indesirable program.)")
+        to_build.append(pkg)
+        
+    print(f"{colors.BOLD}==> {colors.END} SUMMARY :\n{len(to_build)} Package(s) to build:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}" for pkg in to_build))
     if not args.noconfirm : 
-        ask = input(f"{colors.BOLD}==>{colors.END} Do you want to continue ? Y/n : ")
-        if ask.lower() == "n":
-            print(f"{colors.RED}==> CANCELED{colors.END} : Build Canceled.")
-            return
-    download_pckg(args.build)
-    os.chdir(f"./{args.build.lower()}")
-    r = subprocess.run(['makepkg', '-s'], text=True, stdout=True)
-    if not r.returncode == 0:
-        print(f"{colors.RED}==> ERROR{colors.END} : Error while running makepkg. \n{r.stdout}")
-        if not args.noconfirm : 
-            ask = input(f"{colors.BOLD}==>{colors.END} Do you want to clean build ? Y/n : ")
+            ask = input(f"{colors.BOLD}==>{colors.END} Do you want to continue ? Y/n : ")
             if ask.lower() == "n":
-                return
-        clean(args.build)
-        return
+                print(f"{colors.RED}==> CANCELED{colors.END} : Build(s) Canceled.")
+                return 
+    for pkg in args.build:    
+        download_pckg(pkg)
+        try:
+            os.chdir(f"./{pkg.lower()}")
+        except Exception as e:
+            print(f"{colors.RED}==> ERROR{colors.END} : {e}")
+        r = subprocess.run(['makepkg', '-s'], text=True, stdout=True)
+        if not r.returncode == 0:
+            print(f"{colors.RED}==> ERROR{colors.END} : Error while running makepkg. \n{r.stdout}")
+            if not args.noconfirm : 
+                ask = input(f"{colors.BOLD}==>{colors.END} Do you want to move build to cache ? Y/n : ")
+                if ask.lower() == "n":
+                    continue
+            move_pkg(pkg)
+            print(f"{colors.GREEN}==>{colors.END} Package {pkg} successfully built.")
+        
     
-    print(f"{colors.GREEN}==>{colors.END} Package {args.build} successfully built.")
+
+def local_install():
+    pkgs = libalpm.alpm.getpkgslist()
+    to_install = []; to_reinstall = []
+    for pkg in args.local_install:
+        pkg_name = "".join(pkg)
+        if not pkg in os.listdir(cache_folder): print(f"{colors.RED}==> ERROR{colors.END} : Package {pkg_name} not found on cache."); return
+        if pkg in pkgs:
+            pkg_version = pkgs[pkg]
+            Database.add_db(pkg, pkg_version)
+            to_reinstall.append((pkg, pkg_version))
+        else:
+            to_install.append((pkg, pkg_version))
+    print(f"{colors.BOLD}==> {colors.END} SUMMARY :\n{len(to_install)} Package(s) to install:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}@{colors.GREEN}{version}{colors.END}" for pkg, version in to_install)+
+        f"\n{len(to_reinstall)} Package(s) to reinstall:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}@{colors.GREEN}{version}{colors.END}" for pkg, version in to_reinstall))
+    if not args.noconfirm : 
+        ask = input(f"{colors.BOLD}==>{colors.END} Do you want to continue installation ? Y/n : ")
+        if ask.lower() == "n":
+            print(f"{colors.RED}==> CANCELED{colors.END} : Installation Canceled.")
+            return
+    for pkg in args.local_install:
+        pkg_name = "".join(pkg)
+        os.chdir(f"{cache_folder}/{pkg_name}")
+        r = subprocess.run(['makepkg', '-si'], text=True, stdout=True)
+        if not r.returncode == 0:
+            print(f"{colors.RED}==> ERROR{colors.END} : Error while running makepkg. \n{r.stdout}")
+            return
+        print(f"{colors.GREEN}==>{colors.END} Successfully installed {pkg_name}")
 
 def main():
+    if args.local_install:
+        local_install()
+        return
     if args.build:
         build_only()
         return
@@ -89,11 +136,14 @@ def main():
     if args.install_random:
         choose_random_pkg()
         return
+    if args.clear_cache:
+        clean_cache()
+        return
     if args.show_installed_aur_pkgs:
         installed_aur_pkgs()
         return 
     if args.nob_version:
-        print(f"{colors.BOLD}Nob {_version}{colors.END}")
+        print(f"{colors.BOLD}Nob {_version}\nPyalpm {_pyalpm_version}{colors.END}")
         return
     if args.aur_upgrade:
         AUR_upgr()
@@ -120,27 +170,36 @@ def main():
     
     if args.install:
         #Find Package
-        result = download_find_pkg(args.install)
-        if result == None: return
-        pkg_version, pkg_popularity = result
-        if pkg_popularity <= 2:
-            print(f"{colors.YELLOW}==> WARNING{colors.END} : This package popularity is LOW ({pkg_popularity}), this may be an indesirable program.)")
-            if not args.noconfirm : 
-                ask = input(f"{colors.BOLD}==>{colors.END} Do you want to continue installation ? Y/n : ")
-                if ask.lower() == "n":
-                    print(f"{colors.RED}==> CANCELED{colors.END} : Installation Canceled.")
-                    return
         pkgs = libalpm.alpm.getpkgslist()
-        # Add the package into db with installed version, not latest version.
-        if args.install in pkgs:
-            installed_pckg_ver = pkgs[args.install]
-            Database.add_db(args.install, installed_pckg_ver)
-            print(f"{colors.YELLOW}==> WARNING{colors.END} : {args.install} is already installed. If you continue the installation, this package will be reinstalled.")
-        
-            
-        download_pckg(args.install); install_pckg(pkg_version, args.install)
+        to_install = []; to_reinstall = []
+        for pkg in args.install:
+            result = download_find_pkg(pkg, True)
+            if result == None: return
+            #Check how many stars the program has.
+            pkg_version, pkg_popularity = result
+            if pkg_popularity <= 2:
+                print(f"{colors.YELLOW}==> WARNING{colors.END} : {pkg} is not very popular. (only {pkg_popularity})")
+            # Add the package into db with installed version, not latest version.
+            if pkg in pkgs:
+                installed_pckg_ver = pkgs[pkg]
+                Database.add_db(pkg, installed_pckg_ver)
+                #print(f"{colors.CYAN}==> INFO{colors.END} : {pkg} is already installed. This package will be reinstalled.") 
+                to_reinstall.append((pkg, pkg_version))
+            else:
+                to_install.append((pkg, pkg_version))
+        print(f"{colors.BOLD}==> {colors.END} SUMMARY :\n{len(to_install)} Package(s) to install:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}@{colors.GREEN}{version}{colors.END}" for pkg, version in to_install)+
+              f"\n{len(to_reinstall)} Package(s) to reinstall:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}@{colors.GREEN}{version}{colors.END}" for pkg, version in to_reinstall))
+        if not args.noconfirm : 
+            ask = input(f"{colors.BOLD}==>{colors.END} Do you want to continue installation ? Y/n : ")
+            if ask.lower() == "n":
+                print(f"{colors.RED}==> CANCELED{colors.END} : Installation Canceled.")
+                return
+        for pkg, pkg_version in to_install:
+            download_pckg(pkg); install_pckg(pkg_version, pkg)
+        for pkg, pkg_version in to_reinstall:
+            download_pckg(pkg); install_pckg(pkg_version, pkg)
 
-def download_find_pkg(pkg):
+def download_find_pkg(pkg, noprint=False):
     d_Find_URL = f"https://aur.archlinux.org/rpc.php?v=5&type=info&arg={pkg}"
     try : 
         with urllib.request.urlopen(f"{d_Find_URL}") as response:
@@ -154,7 +213,7 @@ def download_find_pkg(pkg):
         return None
     #get basic info
     pkg_version = rep['results'][0]['Version']; pkg_maintainer = rep['results'][0]['Maintainer']; pkg_popularity = rep['results'][0]['Popularity']
-    print(f"{colors.GREEN}==>{colors.END} Package found for query : {pkg}/{pkg_version} by {pkg_maintainer} .\n")
+    if not noprint : print(f"{colors.GREEN}==>{colors.END} Package found for query : {pkg}/{pkg_version} by {pkg_maintainer} .\n")
     return pkg_version, pkg_popularity
 
 def download_pckg(pkg):
@@ -188,7 +247,7 @@ def install_pckg(pkg_version, pkg):
             try: 
                 input(f"{colors.BOLD}==>{colors.END} Press any key to continue installation.\nCTRL+C to cancel installation.\n")
             except KeyboardInterrupt:
-                return clean(pkg)
+                return move_pkg(pkg)
 
     print(f"{colors.CYAN}==>{colors.END} Executing makepkg")
     if not args.noconfirm:
@@ -197,24 +256,30 @@ def install_pckg(pkg_version, pkg):
         r = subprocess.run(['makepkg', '-si', '--noconfirm'], text=True, stdout=True)
     if not r.returncode == 0:
         print(f"{colors.RED}==> ERROR{colors.END} : Error while running makepkg. \n{r.stdout}")
-        clean(pkg)
+        move_pkg(pkg)
         return
-    clean(pkg)
+    move_pkg(pkg)
     #Add the pkg to db (if already exit, editing it with new version)
     Database.add_db(pkg, pkg_version)
 
-def clean(pkg):
-    print(f"{colors.CYAN}==>{colors.END} Cleaning installation...")
+def move_pkg(pkg):
+    os.system("sudo mkdir -p /var/cache/nob/pkgs")
+    print(f"{colors.CYAN}==>{colors.END} Moving ./{pkg.lower()} to cache.")
     os.chdir("../")
-    try:
-        os.system(f"sudo rm -rf ./{pkg.lower()}")
+    os.system(f"sudo mv ./{pkg.lower()} {cache_folder}")
+
+def clean_cache():
+    print(f"{colors.CYAN}==>{colors.END} Cleaning cache...")
+    try: 
+        os.system(f"sudo rm -rf {cache_folder}/*")
     except Exception as e:
-        print(f"{colors.RED}==> ERROR{colors.END} : Error while cleaning :{e}.")
+        print(f"{colors.RED}==> ERROR{colors.END} : Error while cleaning cache. Please do it yourself. [{e}]")
         return
+    print(f"{colors.GREEN}==>{colors.END} Cache sucessfully cleaned.")
 
 def get_aur_packages_list():
     packages = []
-    #Downloading && extacting packages list from AUR
+    #Downloading && extacting packages list from AUR    
     if not os.path.exists("/tmp/packages"):
         print(f"{colors.CYAN}==>{colors.END} Fetching AUR packages list...")
         r = subprocess.run("curl --retry 3 -s -o /tmp/packages.gz https://aur.archlinux.org/packages.gz", shell=True, text=True, stdout=subprocess.PIPE)
@@ -246,20 +311,23 @@ def remove_pckg():
     if '-Rs' in sys.argv: flag = '-Rs'
     if '-Rn' in sys.argv:flag = '-Rn'
     #Removing package.
-    if not args.remove in libalpm.alpm.getpkgslist():
-        print(f"{colors.RED}==> ERROR{colors.END} : Cannot delete {args.remove} because it isn't installed yet. ")
-        Database.remove_db(args.remove)
-        return
-    print(f"{colors.CYAN}==>{colors.END} Removing {args.remove}...")
+    to_remove = []
+    for pkg in args.remove:
+        if not pkg in libalpm.alpm.getpkgslist():
+            print(f"{colors.RED}==> ERROR{colors.END} : Cannot delete {pkg} because it isn't installed yet. ")
+            Database.remove_db(pkg)
+            return
+        to_remove.append(pkg)
+    print(f"{colors.BOLD}==> {colors.END} SUMMARY :\n{len(to_remove)} Package(s) to remove:\n" + " ".join(f"{colors.CYAN}{pkg}{colors.END}" for pkg in to_remove))
     if not args.noconfirm:
-        r = subprocess.run(['sudo', 'pacman', flag, args.remove], text=True, stdout=True)
+        r = subprocess.run(['sudo', 'pacman', flag, " ".join(pkg for pkg in to_remove)], text=True, stdout=True)
     else:
         r = subprocess.run(['sudo', 'pacman', '--noconfirm', flag, args.remove], text=True, stdout=True)
     if not r.returncode == 0:
         print(f"{colors.RED}==> ERROR{colors.END} : An error has occured while deleting the package {args.remove}.")
         return
     print(f"{colors.GREEN}==>{colors.END} Package {args.remove} successfully deleted from the system.")
-    Database.remove_db(args.remove)
+    for pkg in args.remove : Database.remove_db(args.remove)
            
 def installed_aur_pkgs():
     print(f"{colors.CYAN}==>{colors.END} Reading nob's DB...")
@@ -306,9 +374,13 @@ def AUR_upgr():
     packages_to_update = []
     # reading db file + Adding Packages in packages var 
     
-    print(f"{colors.CYAN}==>{colors.END} Checking for AUR packages updates...")
+    print(f"{colors.CYAN}==>{colors.END} Checking for AUR packages updates... [0%] (0/{len(packages)})", end='', flush=True)
     #Check for updates && check if package is on th AUR.
+    i=0
     for pkg_name, pkg_ver in packages:
+        i +=1; percent = round(i / len(packages) * 100)
+        if percent >= 100: print(f"\r{colors.CYAN}==>{colors.END} Checking for AUR packages updates... [{percent}%] ({i}/{len(packages)})", end='\n', flush=True)
+        else : print(f"\r{colors.CYAN}==>{colors.END} Checking for AUR packages updates... [{percent}%] ({i}/{len(packages)})", end='', flush=True)
         try:
             with urllib.request.urlopen(f"https://aur.archlinux.org/rpc.php?v=5&type=info&arg={pkg_name}") as response:
                 data = response.read().decode("utf-8")
@@ -323,11 +395,13 @@ def AUR_upgr():
             print(f"{colors.RED}==> ERROR{colors.END} : DATABASE corrupted. Manual repair needed (PACKAGE {pkg_name} found in database but not in AUR).")
             return
         if pkg_version != pkg_ver:
-            print(f"{colors.CYAN}==>{colors.END} Available update found : {pkg_name}/{colors.RED}{pkg_ver}{colors.END} ==> {pkg_name}/{colors.GREEN}{pkg_version}{colors.END}.")
             packages_to_update.append({
                 "result_name": result_name,
                 "pkg_version": pkg_version
             })
+    for pkg in packages_to_update:
+        pkg_name = pkg['result_name']; pkg_version = pkg["pkg_version"]
+        print(f"{colors.CYAN}==>{colors.END} {pkg_name}/{colors.RED}{pkg_ver}{colors.END} ==> {pkg_name}/{colors.GREEN}{pkg_version}{colors.END}.")
     
     if len(packages_to_update) > 0:
         if not args.noconfirm:
